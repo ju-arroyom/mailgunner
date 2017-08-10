70541fdc331e29f01b09fb4baf65ace4b7875058
Juan Arroyo <juan.arroyo@avant.com>
2017-08-10 15:30:57 -0500

    Email body can be an html

--- R/send_email.R
+++ R/send_email.R
@@ -8,17 +8,24 @@
 #'   \code{c("Somebody Amazing", "someone") --> "Somebody Amazing <someone@domain.com>"}
 #' @param to character. Email addresses of recipients.
 #' @param subject character. Email subject.
-#' @param text character. Email text.
+#' @param content character. Email content.
 #' @param tag character. Mailgun tags (I still don't really know what this is but why not).
 #' @param attach character. Filenames of your attachments.
+#' @param format character. Type of format desired. Options are text or html.
 #' @export
-send_email <- function(from, to, subject, text, attach = NULL, tag = NULL) {
+send_email <- function(from, to, subject, content, attach = NULL, tag = NULL, format = c("text", "html")) {
   if (length(from) == 1) {
     from <- paste0(from, "@", mailgun_domain_name())
   } else {
     from <- paste0(from[1], " <", from[2], "@", mailgun_domain_name(), ">")
   }
-  payload <- list(from = from, subject = subject, text = text)
+  format_type <- match.arg(format)
+
+  if(format_type != "text") {
+    payload <- list(from = from, subject = subject, html = content)
+  } else {
+    payload <- list(from = from, subject = subject, text = content)
+  }
   payload <- append(payload, `names<-`(lapply(to, identity), rep("to", length(to))))
   if (!is.null(attach)) {
     payload <- append(payload, `names<-`(lapply(attach, httr::upload_file), rep("attachment", length(attach))))
